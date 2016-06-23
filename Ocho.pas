a11bcb02f67ce3edf1e0e1f9a2e289c515a3e6e7
Román Girard <Romn Girard>
2016-06-23 17:15:56 -0300

    ocho corregido

--- Ocho.pas
+++ Ocho.pas
@@ -10,20 +10,31 @@ var
     
     
 begin
-writeln('ingrese la cantidad de numeros');
-readln(N);
-writeln('ingrese el primer numero');
-readln(Nro1);
-writeln('ingrese el siguiente numero');
-Min := Nro1;
-readln(Nro2);
-C := 2;
+  writeln('ingrese la cantidad de numeros');
+  readln(N);
+  writeln('ingrese el primer numero');
+  readln(Nro1);
+  writeln('ingrese el siguiente numero');
+  Min := Nro1;
+  readln(Nro2);
+  C := 2;
         while (Nro2 > Min) and (C < N) do
           begin
             writeln('ingrese el siguiente numero');  
             readln(Nro2);
             C := C + 1;
           end;
+
+        if (Nro2 > Min) then
+          begin
+            Write('el primer numero es el minimo');
+          end
+          else
+          begin
+            Write('el primer numero no es el minimo');
+          end;
+
+
   
   
 end.
