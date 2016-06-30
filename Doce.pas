b4d14b24caf0b226307518d321664d203e61bd76
Román Girard <Romn Girard>
2016-06-30 15:50:17 -0300

    ultima version

--- Doce.pas
+++ Doce.pas
@@ -2,22 +2,41 @@ program doce;
 
 var
    
-    N : integer;
-    Nro : integer;
-    Max : integer;
-   
     
+    Nro1 : integer;
+    Nro2 : integer;
+    Max : integer;
+    AM  : integer; //anterior al maximo (AM)
     
 begin
-
+Max :=0;
+AM :=0;
 writeln('ingrese el primer numero');
-readln(Nro);
-Max := Nro;
+readln(Nro1);
 
-while (Nro <> 0) and (Nro <= Max) do
+while (Nro1 <> 0) do
     begin
-        writeln('ingrese el siguiente numero');  
-        readln(Nro);       
+        if Nro1 > Max then
+          begin
+            AM := Max;
+            Max := Nro1;
+          end
+          else
+            begin
+              if Nro1 > Am then
+              begin
+                AM := Nro1;
+              end;
+          end;
+
+        writeln('ingrese otro numero');  
+        readln(Nro1);
     end;
 
-end.
+writeln('el maximo y el siguiente mas grande respectivamente son:');
+writeln(Max , AM);
+  
+ end.
+    
+
+
